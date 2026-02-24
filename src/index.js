c0473fbc22c33d27bf90c96bcd4ef315a5f12fe9
thedittya471 <dittyamaity@gmail.com>
2026-02-24 23:34:52 +0530

    added utils

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import dotenv from 'dotenv'
-import connectDB from './db/index.js'
-import { connect } from 'mongoose'
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { connect } from "mongoose";
 
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongoDb connection failed :", err);
+  });
